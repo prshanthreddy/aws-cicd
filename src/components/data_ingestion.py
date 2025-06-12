e747ec03281e3a0555b55b5279290b1421ba8558
PRASHANTH REDDY <81088623+prshanthreddy@users.noreply.github.com>
2025-06-11 22:33:04 -0500

    Update data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,7 +2,7 @@ import os
 import sys
 from src.exception import CustomException
 from src.logger import logging
-import pandas as pd
+import pandas as pd 
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
